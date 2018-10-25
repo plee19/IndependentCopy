d13bbb1dca9f8f5bc27cdd94e8eaabbae421c734
plee19 <plee19@my.wctc.edu>
2018-10-24 20:28:23 -0500

    Javadoc completed

--- src/independentcopy/ListReader.java
+++ src/independentcopy/ListReader.java
@@ -2,12 +2,26 @@ package independentcopy;
 
 import java.util.ArrayList;
 
+/**
+ * Reads through a designated ArrayList. Implements Reader interface.
+ * @author Elijah Ligrow, Andre Araujo, Paul Lee
+ * @version 1.0
+ */
 public class ListReader implements Reader {
     ArrayList<String> arrayList;
 
+    /**
+     * Constructor method for ListReader. Takes ArrayList to use in further methods
+     * @param someList ArrayList<String>: ArrayList to base further methods upon
+     */
     public ListReader(ArrayList<String> someList) {
         this.arrayList = someList;
     }
+
+    /**
+     * Reads values in ArrayList. Overrides from Reader.
+     * @return String: String value in ArrayList
+     */
     @Override
     public String readln() {
         return arrayList.get(arrayList.size() - 1);
--- src/independentcopy/ListWriter.java
+++ src/independentcopy/ListWriter.java
@@ -2,17 +2,35 @@ package independentcopy;
 
 import java.util.ArrayList;
 
+/**
+ * Writes to a designated ArrayList. Implements Writer interface.
+ * @author Elijah Ligrow, Andre Araujo, Paul Lee
+ * @version 1.0
+ */
+
 public class ListWriter implements Writer {
     ArrayList<String> arrayList;
 
+    /**
+     * Constructor for ListWriter. Takes ArrayList to use in further methods
+     * @param arrayList ArrayList<String>: ArrayList to base further methods upon
+     */
     public ListWriter(ArrayList<String> arrayList) {
         this.arrayList = arrayList;
     }
 
+    /**
+     * Writes string value to ArrayList
+     * @param str String: value to write to ArrayList
+     */
     public void writeln(String str) {
         arrayList.add(str);
     }
 
+    /**
+     * Returns the ArrayList
+     * @return ArrayList<String>: ArrayList to return
+     */
     public ArrayList<String> getArrayList() {
         return arrayList;
     }
